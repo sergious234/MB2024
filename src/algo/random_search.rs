e0c6ae71b92a131cf7f76ad9fec7c08edfeeed48
sergious234 <sergious234@gmail.com>
2024-03-11 20:30:03 +0100

    Bit of refactor

--- src/algo/random_search.rs
+++ src/algo/random_search.rs
@@ -17,13 +17,12 @@ impl<'a> RandomSearch<'a> {
     }
 
     pub fn run(&self) -> usize {
-        let mut sol = self.gen_sol();
         let mut it = 0;
 
-        let mut best_sol = sol;
+        let mut best_sol = self.gen_sol();
         let mut best_cost = cost(&self.cost_mat, &best_sol);
         while it < N_EVAL {
-            sol = self.gen_sol();
+            let sol = self.gen_sol();
             let new_cost = cost(&self.cost_mat, &sol);
 
             if new_cost < best_cost {
@@ -42,12 +41,15 @@ impl<'a> RandomSearch<'a> {
 
     fn gen_sol(&self) -> Trucks {
         let mut new_sol = Trucks::default();
-        for pal in self.palets.iter().cloned() {
+        for i in 0..new_sol.len() {
+            new_sol[i] = Vec::with_capacity(TRUCK_CAP);
+        }
+        for pal in self.palets.iter() {
             let mut to_truck = rng::next_usize() % N_TRUCKS;
             while new_sol[to_truck].len() >= TRUCK_CAP {
                 to_truck = rng::next_usize() % N_TRUCKS;
             }
-            new_sol[to_truck].push(pal);
+            new_sol[to_truck].push(*pal);
         }
         new_sol
     }
